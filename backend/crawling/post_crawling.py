1cca716912eb26a65fb4d048833a1f6b23479218
Divison Officer <wlssd0701@naver.com>
2023-09-16 15:13:16 +0900

    [23.09.16] 목록 크롤러, pandas 적용

--- backend/crawling/post_crawling.py
+++ backend/crawling/post_crawling.py
@@ -1,33 +1,6 @@
 import requests
+import re
 from bs4 import BeautifulSoup
-import nltk
-nltk.download('punkt')
-
- # Download the Punkt tokenizer data (only need to do this once)
-
-def summarize_korean_text(korean_text):
-    # Tokenize the text into sentences
-    sentences = nltk.sent_tokenize(korean_text, language='korean')
-
-    # Summarize the text
-    summary = ""
-    current_length = 0
-
-    for sentence in sentences:
-        # Calculate the length of the current sentence (considering Korean characters' length)
-        sentence_length = len(sentence.encode('utf-8').decode('utf-8'))
-
-        # If adding the current sentence would exceed 100 characters, break the loop
-        if current_length + sentence_length > 100:
-            break
-
-        # Otherwise, add the sentence to the summary and update the current length
-        summary += sentence
-        current_length += sentence_length
-
-    return summary
-
-
 
 
 def get_html(url):
@@ -38,17 +11,53 @@ def get_html(url):
     return _html
 
 def get_post_content(articleNo):
-    html_content = get_html("https://sw.skku.edu/sw/notice.do?mode=view&articleNo={0}".format(articleNo))
-    soup = BeautifulSoup(html_content, 'html.parser')
-    soup_title = soup.find('div', {'class': 'board-view-title-wrap'})
-    post_category = soup_title.find('span').text.strip()
-    post_title = soup_title.find('h4').text[len(post_category) + 1:].strip()
-    soup_content = soup.find('div', {'class': 'fr-view'}).text.strip()
-    content_summary = summarize_korean_text(soup_content)
-    print("Post Category : ", post_category)
-    print("Post Title : ", post_title)
-    print("Post Content : ", soup_content)
-    print("Post Summary : ", content_summary)
+    try:
+        html_content = get_html("https://sw.skku.edu/sw/notice.do?mode=view&articleNo={0}".format(articleNo))
+        soup = BeautifulSoup(html_content, 'html.parser')
+        soup_title = soup.find('div', {'class': 'board-view-title-wrap'})
+
+
+        post_category = soup_title.find('span').text.strip()
+        if not (post_category.startswith('[') and post_category.endswith(']')):
+            post_category = "[기타]"
+        
+        
+        post_date = soup_title.find_all('li')[2].text.strip()
+        post_title = soup_title.find('h4').text[len(post_category) + 1:].strip()
+
+
+
+        try:
+
+            soup_content = soup.find('div', {'class': 'fr-view'}).text.strip()
+        except AttributeError:
+            soup_content = soup.find('div', {'class':'board-view-content-wrap board-view-txt'}).text.strip()
+
+            
+        url_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+        
+        soup_content = url_pattern.sub('', soup_content)
+        
+        
+        soup_content = re.sub(r'[^\uAC00-\uD7A3a-zA-Z0-9 ]', '', soup_content)
+        content_summary = soup_content
+    except AttributeError:
+        print("Article {0} Format error".format(articleNo))
+        print(soup_content)
+        return None
+    #print("Post Category : ", post_category)
+    #print("Post Title : ", post_title)
+    #print("Post Content : ", soup_content)
+    #print("Post Summary : ", content_summary)
+
+
+    return {
+        'post_title': post_title,
+        'post_category': post_category,
+        'post_content': soup_content,
+        'post_date': post_date,
+        'post_url' : "https://sw.skku.edu/sw/notice.do?mode=view&articleNo={0}".format(articleNo)
+    }
 
 
 
